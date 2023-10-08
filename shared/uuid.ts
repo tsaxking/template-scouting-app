9aefa2b277505a543e635927bfd633bd24bb5f00
tsaxking <taylor.reese.king@gmail.com>
2023-10-08 17:24:01 -0600

    uuid

---
+++ shared/uuid.ts
@@ -0,0 +1,14 @@
+export const random = (options?: {
+    length?: number;
+    charset?: string;
+}) => {
+    const length = options?.length ?? 16;
+    const charset = options?.charset ?? 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+
+    return result;
+};
