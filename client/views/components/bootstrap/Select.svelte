22daaaa16c64ee0c6c8c2af7ba932c19af7ab58e
Taylor King <taylorking206@u.boisestate.edu>
2023-12-06 12:18:49 -0700

    handle change

--- client/views/components/bootstrap/Select.svelte
+++ client/views/components/bootstrap/Select.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
     export let options: string[] = [];
     export let value: string = '';
+
+    const dispatch = createEventDispatcher();
+
+    const handleChange = (event: Event) => {
+        const target = event.target as HTMLSelectElement;
+        dispatch('change', target.value);
+    }
 </script>
 
-<select bind:value>
+<select bind:value on:change={handleChange}>
     {#each options as option}
         <option value={option}>{option}</option>
     {/each}
