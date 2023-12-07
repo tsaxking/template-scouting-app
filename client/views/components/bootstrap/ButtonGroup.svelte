01731bbd5bb9baefd9458f00d701040fce0d8527
tsaxking <taylorking206@u.boisestate.edu>
2023-12-07 09:32:49 -0700

    button group

---
+++ client/views/components/bootstrap/ButtonGroup.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+
+    export let size: 'sm' | 'lg' = 'sm';
+    export let vertical: boolean = false;
+
+</script>
+
+
+
+<div class="btn-group btn-group-{size} {vertical ? 'btn-group-vertical' : ''}" role="group" aria-label="Basic example">
+    <slot></slot>
+</div>
