f0928528b1e350d49a9468bc6a4e912b7008b9eb
Taylor King <taylorking206@u.boisestate.edu>
2024-01-04 14:11:48 -0700

    change app structure

--- client/models/canvas/circle.ts
+++ client/models/canvas/circle.ts
@@ -1,7 +1,8 @@
 import { Point2D } from "../../submodules/calculations/src/linear-algebra/point";
+import { ShapeProperties } from "./shape-properties";
 
 export class Circle {
-    constructor(public center: Point2D, public radius: number) {}
+    constructor(public center: Point2D, public radius: number, public properties?: ShapeProperties) {}
 
     isIn(x: number, y: number) {
         return Math.sqrt(Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2)) < this.radius;
@@ -10,7 +11,11 @@ export class Circle {
     draw(context: CanvasRenderingContext2D) {
         context.save();
         context.beginPath();
+        if (this.properties?.line?.color) context.strokeStyle = this.properties.line.color;
+        if (this.properties?.line?.width) context.lineWidth = this.properties.line.width;
+        if (this.properties?.fill?.color) context.fillStyle = this.properties.fill.color;
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        if (this.properties?.fill) context.fill();
         context.stroke();
         context.restore();
     }
--- client/models/canvas/path.ts
+++ client/models/canvas/path.ts
@@ -1,12 +1,15 @@
 import { Point, Point2D } from "../../submodules/calculations/src/linear-algebra/point";
+import { LineProperties } from "./shape-properties";
 
 export class Path {
-    constructor(public points: Point2D[]) {}
+    constructor(public points: Point2D[], public properties?: Partial<LineProperties>) {}
 
     draw(context: CanvasRenderingContext2D) {
         context.save();
         context.beginPath();
         context.moveTo(this.points[0][0], this.points[0][1]);
+        if (this.properties?.color) context.strokeStyle = this.properties.color;
+        if (this.properties?.width) context.lineWidth = this.properties.width;
         for (let i = 1; i < this.points.length; i++) {
             context.lineTo(this.points[i][0], this.points[i][1]);
         }
