ca83f69fb9bddfcb52b4372eeb0a7b26a8cfe4b9
tsaxking <taylorking206@u.boisestate.edu>
2024-01-09 11:58:43 -0700

    structure and document

--- client/models/canvas/canvas.ts
+++ client/models/canvas/canvas.ts
@@ -1,80 +1,302 @@
-// type Events = 
-//     'click' |
-//     'mousedown' |
-//     'mouseup' |
-//     'mousemove' |
-//     'mouseleave' |
-//     'touchstart' |
-//     'touchend' |
-//     'touchmove' |
-//     'touchcancel';
 
+
+import { EventEmitter } from "../../../shared/event-emitter";
+import { Point2D } from "../../submodules/calculations/src/linear-algebra/point";
+
+
+/**
+ * Similar to mouse events, but with a point
+ * @date 1/9/2024 - 11:39:34 AM
+ *
+ * @export
+ * @class CanvasEvent
+ * @typedef {CanvasEvent}
+ * @template T (MouseEvent | TouchEvent)
+ */
+export class CanvasEvent<T> {
+    /**
+     * Time of the event
+     * @date 1/9/2024 - 11:40:14 AM
+     *
+     * @public
+     * @readonly
+     * @type {Date}
+     */
+    public readonly time: Date = new Date();
+
+    /**
+     * Creates an instance of CanvasEvent.
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @constructor
+     * @param {keyof Events} event
+     * @param {Point2D} point
+     * @param {T} data
+     */
+    constructor(
+        public readonly event: keyof Events,
+        public readonly point: Point2D,
+        public readonly data: T
+    ) {}
+};
+
+/**
+ * All events for the canvas
+ * @date 1/9/2024 - 11:39:34 AM
+ *
+ * @typedef {Events}
+ */
+type Events = {
+    click: CanvasEvent<MouseEvent | TouchEvent>;
+};
+
+/**
+ * An interface for all drawable objects
+ * @date 1/9/2024 - 11:39:34 AM
+ *
+ * @export
+ * @interface Drawable
+ * @typedef {Drawable}
+ */
 export interface Drawable {
+    /**
+     * The draw function
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @param {CanvasRenderingContext2D} ctx
+     */
     draw(ctx: CanvasRenderingContext2D): void;
 
-    // on?(event: string, callback: (event: Event) => void): void;
+    /**
+     * Event emitter for the drawable, if it has one
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @readonly
+     * @type {?EventEmitter<keyof Events>}
+     */
+    readonly $emitter?: EventEmitter<keyof Events>;
+
+    /**
+     * Determines if the given point is inside the drawable
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @param {Point2D} point
+     * @returns {boolean}
+     */
+    isIn?(point: Point2D): boolean;
+
+    /**
+     * Emits an event
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @template {keyof Events} K
+     * @param {K} event
+     * @param {Events[K]} data
+     */
+    emit?<K extends keyof Events>(event: K, data: Events[K]): void;
+    /**
+     * Adds an event listener
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @template {keyof Events} K
+     * @param {K} event
+     * @param {(e: Events[K]) => void} cb
+     */
+    on?<K extends keyof Events>(event: K, cb: (e: Events[K]) => void): void;
+    /**
+     * Removes an event listener
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @template {keyof Events} K
+     * @param {K} event
+     * @param {(e: Events[K]) => void} cb
+     */
+    off?<K extends keyof Events>(event: K, cb: (e: Events[K]) => void): void;
 }
 
+
+
+/**
+ * The canvas class, this contains elements that can be drawn (drawables), event emitters, and more
+ * All points used should be normalized (between 0 and 1)
+ * @date 1/9/2024 - 11:39:34 AM
+ *
+ * @export
+ * @class Canvas
+ * @typedef {Canvas}
+ */
 export class Canvas {
-    public readonly elements: Drawable[] = [];
+    /**
+     * All drawables on the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @public
+     * @readonly
+     * @type {Drawable[]}
+     */
+    public readonly drawables: Drawable[] = [];
+    /**
+     * Event emitter for the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @public
+     * @readonly
+     * @type {*}
+     */
+    public readonly $emitter = new EventEmitter<keyof Events>();
+    /**
+     * Determines if the canvas is animating
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @private
+     * @type {boolean}
+     */
     private animating: boolean = false;
 
     // TODO: Implement FPS
     // public fps: number = 0;
     
+    /**
+     * Creates an instance of Canvas.
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @constructor
+     * @param {CanvasRenderingContext2D} ctx
+     */
     constructor(public readonly ctx: CanvasRenderingContext2D) {
+        ctx.canvas.addEventListener('click', (e) => {
+            this.$emitter.emit('click', e);
+            const [p] = this.getXY(e);
+
+            for (const el of this.drawables) {
+                if (el.$emitter && el.isIn?.(p)) {
+                    el.emit?.('click', new CanvasEvent('click', p, e));
+                }
+            }
+        });
     }
 
+
+
+    // ▄▀  ██▀ ▀█▀ ▀█▀ ██▀ █▀▄ ▄▀▀       ▄▀▀ ██▀ ▀█▀ ▀█▀ ██▀ █▀▄ ▄▀▀ 
+    // ▀▄█ █▄▄  █   █  █▄▄ █▀▄ ▄█▀       ▄█▀ █▄▄  █   █  █▄▄ █▀▄ ▄█▀ 
+
+
+    /**
+     * Width of the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @type {number}
+     */
     get width() {   
         return this.ctx.canvas.width;
     }
 
-    get height() {
-        return this.ctx.canvas.height;
-    }
-
+    /**
+     * Sets the width of the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @type {number}
+     */
     set width(width: number) {
         this.ctx.canvas.width = width;
     }
 
+    /**
+     * Height of the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @type {number}
+     */
+    get height() {
+        return this.ctx.canvas.height;
+    }
+
+    /**
+     * Sets the height of the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @type {number}
+     */
     set height(height: number) {
         this.ctx.canvas.height = height;
     }
-
-    add(...elements: Drawable[]) {
-        this.elements.push(...elements);
+    
+    /**
+     * Image data of the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @type {ImageData}
+     */
+    get imageData() {
+        return this.ctx.getImageData(0, 0, this.width, this.height);
     }
 
-    remove(...elements: Drawable[]) {
-        for (const element of elements) {
-            const index = this.elements.indexOf(element);
-            if (index !== -1) this.elements.splice(index, 1);
+    /**
+     * Sets the image data of the canvas 
+     * This will clear the canvas, and stop animating
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @type {ImageData}
+     */
+    set imageData(imageData: ImageData) {
+        this.animating = false;
+        this.clear();
+        this.ctx.putImageData(imageData, 0, 0);
+    }
+
+
+
+
+    /**
+     * Adds drawables to the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @param {...Drawable[]} drawables
+     */
+    add(...drawables: Drawable[]) {
+        this.drawables.push(...drawables);
+    }
+
+    /**
+     * Removes drawables from the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @param {...Drawable[]} drawables
+     */
+    remove(...drawables: Drawable[]) {
+        for (const drawable of drawables) {
+            const index = this.drawables.indexOf(drawable);
+            if (index !== -1) this.drawables.splice(index, 1);
         }
     }
 
+    /**
+     * Clears the canvas
+     * @date 1/9/2024 - 11:39:34 AM
+     */
     clear() {
         this.ctx.clearRect(0, 0, this.width, this.height);
     }
 
+    /**
+     * Iterates through all drawables and calls their draw function
+     * @date 1/9/2024 - 11:39:34 AM
+     */
     draw() {
-        for (const element of this.elements) {
+        for (const element of this.drawables) {
             this.ctx.save();
             element.draw(this.ctx);
             this.ctx.restore();
         }
     }
 
-    get imageData() {
-        return this.ctx.getImageData(0, 0, this.width, this.height);
-    }
-
-    set imageData(imageData: ImageData) {
-        this.ctx.putImageData(imageData, 0, 0);
-    }
-
     /**
      * Animates the canvas (calls draw() every frame)
      * To change the animation, change the elements array outside of this class
+     * @date 1/9/2024 - 11:39:34 AM
+     * 
+     * @param {(canvas: this) => void} [update] - The update function, called every frame (optional)
      */
     animate(update?: (canvas: this) => void): () => void {
         const stop = () => this.animating = false;
@@ -93,9 +315,68 @@ export class Canvas {
         return stop;
     }
 
+    /**
+     * Clears the canvas, stops animation, and removes all drawables
+     * @date 1/9/2024 - 11:39:34 AM
+     */
     destroy() {
         this.clear();
         this.animating = false;
-        this.elements.length = 0;
+        this.drawables.length = 0;
+    }
+
+    /**
+     * Gets the x and y coordinates of the event
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @param {(MouseEvent | TouchEvent)} e
+     * @returns {Point2D[]}
+     */
+    getXY(e: MouseEvent | TouchEvent): Point2D[] {
+        const rect = this.ctx.canvas.getBoundingClientRect();
+        if (e instanceof MouseEvent) {
+            return [[e.clientX - rect.left, e.clientY - rect.top]];
+        } else {
+            return Array.from(e.touches).map((touch) => ([touch.clientX - rect.left, touch.clientY - rect.top]));
+        }
+    }
+
+
+    /**
+     * Adds an event listener
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @template {keyof Events} K
+     * @param {K} event
+     * @param {(e: Events[K]) => void} cb
+     * @returns {void) => void}
+     */
+    on<K extends keyof Events>(event: K, cb: (e: Events[K]) => void) {
+        this.$emitter.on(event, cb);
+    }
+
+    /**
+     * Removes an event listener
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @template {keyof Events} K
+     * @param {K} event
+     * @param {(e: Events[K]) => void} cb
+     * @returns {void) => void}
+     */
+    off<K extends keyof Events>(event: K, cb: (e: Events[K]) => void) {
+        this.$emitter.off(event, cb);
+    }
+
+    /**
+     * Emits an event
+     * @date 1/9/2024 - 11:39:34 AM
+     *
+     * @template {keyof Events} K
+     * @param {K} event
+     * @param {Events[K]} data
+     */
+    emit<K extends keyof Events>(event: K, data: Events[K]) {
+        this.$emitter.emit(event, data);
     }
 }
