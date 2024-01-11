8934fefdfd4815493dae0f2243130904755f3307
tsaxking <taylorking206@u.boisestate.edu>
2024-01-11 02:05:02 -0700

    rm static $emitter from Cache

--- client/models/cache.ts
+++ client/models/cache.ts
@@ -30,67 +30,6 @@ type Updates = 'create' | 'update' | 'delete' | 'archive' | 'restore' | '*';
  */
 export class Cache<data = any> {
     /**
-     * Event emitter for global updates (create, update, delete, archive, restore, etc.)
-     * @date 10/12/2023 - 1:04:42 PM
-     *
-     * @private
-     * @static
-     * @readonly
-     * @type {EventEmitter<Updates>}
-     */
-    private static readonly $emitter: EventEmitter<Updates> = new EventEmitter<
-        Updates
-    >();
-
-    /**
-     * Add a listener for global updates
-     * @date 10/12/2023 - 1:04:42 PM
-     *
-     * @public
-     * @static
-     * @template {Updates} K
-     * @param {K} event
-     * @param {(data: any) => void} callback
-     */
-    public static on<K extends Updates>(
-        event: K,
-        callback: (data: any) => void,
-    ): void {
-        Cache.$emitter.on(event, callback);
-    }
-
-    /**
-     * Remove a listener for global updates
-     * @date 10/12/2023 - 1:04:42 PM
-     *
-     * @public
-     * @static
-     * @template {Updates} K
-     * @param {K} event
-     * @param {?(data: any) => void} [callback]
-     */
-    public static off<K extends Updates>(
-        event: K,
-        callback?: (data: any) => void,
-    ): void {
-        Cache.$emitter.off(event, callback);
-    }
-
-    /**
-     * Emit a global update
-     * @date 10/12/2023 - 1:04:42 PM
-     *
-     * @public
-     * @static
-     * @template {Updates} K
-     * @param {K} event
-     * @param {*} data
-     */
-    public static emit<K extends Updates>(event: K, data: any): void {
-        Cache.$emitter.emit(event, data);
-    }
-
-    /**
      * Cache for storing data (any)
      * @date 10/12/2023 - 1:04:42 PM
      *
