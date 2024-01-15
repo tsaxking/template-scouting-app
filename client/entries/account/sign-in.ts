2ca26791556705b92388ac6fc9e268e573395dd3
Taylor King <taylorking206@u.boisestate.edu>
2024-01-15 16:40:46 -0700

    tatorscout-calculations

--- client/entries/account/sign-in.ts
+++
@@ -1,9 +0,0 @@
-import '../../utilities/imports';
-import SignIn from '../../views/components/SignIn.svelte';
-
-const app = new SignIn({
-    target: document.body,
-    props: {
-        title: 'Team Tators',
-    },
-});
--- client/entries/account/sign-up.ts
+++
@@ -1,9 +0,0 @@
-import '../../utilities/imports';
-import SignUp from '../../views/components/SignUp.svelte';
-
-const app = new SignUp({
-    target: document.body,
-    props: {
-        title: 'Team Tators',
-    },
-});
