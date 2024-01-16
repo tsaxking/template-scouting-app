f4e6c5ace6f931ffc54dec36729cfdcb9554690c
Taylor King <taylorking206@u.boisestate.edu>
2024-01-16 12:26:12 -0700

    eslintignore

--- scripts/fmt-lint-check.sh
+++ scripts/fmt-lint-check.sh
@@ -1,1 +1,1 @@
-npx prettier . --write && deno fmt . && npx eslint . --ignore-pattern ./node_modules/ --ignore-pattern **/*.js --ignore-pattern ./dist/ && deno test --unstable --allow-all
+npx prettier . --write && deno fmt . && npx eslint **/*.ts --ignore-pattern ./node_modules/ --ignore-pattern **/*.js --ignore-pattern ./dist/ && deno test --unstable --allow-all
