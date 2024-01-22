73b858a890e4bcbdbdaea412dc4929bdc6d2ba20
tsaxking <taylorking206@u.boisestate.edu>
2024-01-22 10:51:01 -0700

    jsdoc attempt/result

--- shared/attempt.ts
+++ shared/attempt.ts
@@ -1,34 +1,97 @@
+/**
+ * Ok Result
+ * @date 1/22/2024 - 2:56:57 AM
+ *
+ * @export
+ * @class Ok
+ * @typedef {Ok}
+ * @template [T=unknown]
+ */
 export class Ok<T = unknown> {
-    public readonly ok = true;
+    /**
+     * Creates an instance of Ok.
+     * @date 1/22/2024 - 2:56:57 AM
+     *
+     * @constructor
+     * @param {T} value
+     */
     constructor(public readonly value: T) {}
 
+    /**
+     * Returns true if the result is Ok
+     * @date 1/22/2024 - 2:56:57 AM
+     *
+     * @returns {this is Ok<T>}
+     */
     isOk(): this is Ok<T> {
         return true;
     }
 
+    /**
+     * Returns true if the result is Err
+     * @date 1/22/2024 - 2:56:57 AM
+     *
+     * @returns {this is Err}
+     */
     isErr(): this is Err {
         return false;
     }
 }
 
+/**
+ * Err Result
+ * @date 1/22/2024 - 2:56:57 AM
+ *
+ * @export
+ * @class Err
+ * @typedef {Err}
+ * @template [E=Error]
+ */
 export class Err<E = Error> {
-    public readonly ok = false;
+    /**
+     * Creates an instance of Err.
+     * @date 1/22/2024 - 2:56:57 AM
+     *
+     * @constructor
+     * @param {E} error
+     */
     constructor(public readonly error: E) {}
 
+    /**
+     * Returns true if the result is Ok
+     * @date 1/22/2024 - 2:56:57 AM
+     *
+     * @returns {this is Ok}
+     */
     isOk(): this is Ok {
         return false;
     }
 
+    /**
+     * Returns true if the result is Err
+     * @date 1/22/2024 - 2:56:57 AM
+     *
+     * @returns {this is Err<E>}
+     */
     isErr(): this is Err<E> {
         return true;
     }
 }
 
+/**
+ * Result, Ok or Err
+ * @date 1/22/2024 - 2:56:57 AM
+ *
+ * @export
+ * @typedef {Result}
+ * @template T
+ * @template [E=Error]
+ */
 export type Result<T, E = Error> = Ok<T> | Err<E>;
 
 /**
- * Surrounds a function with a try/catch block and returns the result of the function or null if an error is thrown
- * @date 1/9/2024 - 12:05:02 PM
+ * Attempts to run a function, returning a Result
+ * @date 1/22/2024 - 2:56:57 AM
  */
 export const attempt = <T = unknown, E = Error>(
     fn: (...params: unknown[]) => T,
@@ -38,18 +101,24 @@ export const attempt = <T = unknown, E = Error>(
         return new Ok(fn());
     } catch (e) {
         if (parseError) {
-            const err = parseError(e);
-            console.warn(err);
-            return new Err(err);
+            const err = attempt(
+                () => parseError(e),
+                (e) => 'Error parsing error: ' + e,
+            );
+            if (err.isOk()) {
+                console.warn(err.value);
+                return new Err(err.value);
+            }
+            console.warn(err.error, e);
+            return new Err(e);
         }
         console.warn(e);
         return new Err(e);
     }
 };
-
 /**
- * Surrounds an async function with a try/catch block and returns the result of the function or null if an error is thrown
- * @date 1/9/2024 - 12:05:02 PM
+ * Attempts to run an async function, returning a Promise<Result>
+ * @date 1/22/2024 - 2:56:56 AM
  *
  * @async
  */
@@ -61,8 +130,16 @@ export const attemptAsync = async <T = unknown, E = Error>(
         return new Ok(await fn());
     } catch (e) {
         if (parseError) {
-            const err = parseError(e);
-            return new Err(err);
+            const err = attempt(
+                () => parseError(e),
+                (e) => 'Error parsing error: ' + e,
+            );
+            if (err.isOk()) {
+                console.warn(err.value);
+                return new Err(err.value);
+            }
+            console.warn(err.error, e);
+            return new Err(e);
         }
         console.warn(e);
         return new Err(e);
