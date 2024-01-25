32b7ffc8e2b84dbdea6e895f04b4dfed8bcb6525
tsaxking <taylorking206@u.boisestate.edu>
2024-01-25 00:28:59 -0700

    stdin

--- server/utilities/utilties.ts
+++ server/utilities/utilties.ts
@@ -3,7 +3,9 @@ import { readLines } from 'https://deno.land/std@0.100.0/io/mod.ts';
 
 export const stdin = new EventEmitter();
 (async () => {
+    if (!Deno.args.includes('--allow-stdin')) return;
     for await (const line of readLines(Deno.stdin)) {
         stdin.emit(line);
+        stdin.emit('data', line);
     }
 })();
