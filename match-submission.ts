d8c79b679add81e1a643410394e82202fd41e2d8
tsaxking <taylorking206@u.boisestate.edu>
2024-01-26 12:32:14 -0700

    validate obj

--- match-submission.ts
+++ match-submission.ts
@@ -8,3 +8,11 @@ export type Match = {
     compLevel: 'pr' | 'qm' | 'qf' | 'sf' | 'f';
     eventKey: string;
 };
+
+export const validateObj = {
+    checks: (v) => Array.isArray(v) && v.every((v) => typeof v === 'string'),
+    comments: (v) => typeof v === 'object' && Object.values(v).every((v) => typeof v === 'string'),
+    matchNumber: 'number',
+    teamNumber: 'number',
+    compLevel: ['pr', 'qm', 'qf', 'sf', 'f'],
+}
