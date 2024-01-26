76cef1c4a1f5ac08a3a5db06220c19522318545d
tsaxking <taylorking206@u.boisestate.edu>
2024-01-25 21:53:27 -0700

    traces

--- trace.ts
+++ trace.ts
@@ -1,9 +1,65 @@
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ *
+ * @export
+ * @typedef {Action2024}
+ */
 export type Action2024 = 'spk' | 'amp' | 'src' | 'trp' | 'clb';
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ *
+ * @export
+ * @typedef {Action2023}
+ */
 export type Action2023 = 'cone' | 'cube' | 'balance' | 'pick';
 
+export type Zones2024 =
+    | 'blue-auto'
+    | 'blue-stage'
+    | 'blue-amp'
+    | 'blue-src'
+    | 'blue-zone'
+    | 'red-auto'
+    | 'red-stage'
+    | 'red-amp'
+    | 'red-src'
+    | 'red-zone';
+
+export type TraceParse2024 = {
+    mobility: boolean;
+    parked: boolean;
+};
+
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ *
+ * @export
+ * @typedef {Action}
+ */
 export type Action = Action2024 | Action2023;
 
+export type Zones = Zones2024;
+
+export type TraceParse = TraceParse2024;
+
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ *
+ * @export
+ * @typedef {P}
+ */
 export type P = [number, number, number, Action | 0];
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ *
+ * @export
+ * @typedef {TraceArray}
+ */
 export type TraceArray = P[];
 
 /**
@@ -15,18 +71,30 @@ export type TraceArray = P[];
 const chars =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|:"<>?`~[]\';./=\\,';
 
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ */
 const compressI = (num: number) => {
     let str = chars[Math.floor(num / chars.length)] + chars[num % chars.length];
     if (str[0] === 'A') str = str.slice(1); // remove leading A, since that is the default
     return str;
 };
 
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ */
 const decompressI = (str: string) => {
     if (str.length === 1) str = 'A' + str; // add leading A, since that is the default
     const index = chars.indexOf(str[0]) * chars.length + chars.indexOf(str[1]);
     return index;
 };
 
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ */
 const compressNum = (num: number) => {
     let str = '';
     while (num > 0) {
@@ -36,6 +104,10 @@ const compressNum = (num: number) => {
     return str;
 };
 
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:49 PM
+ */
 const decompressNum = (str: string) => {
     let num = 0;
     for (let i = 0; i < str.length; i++) {
@@ -44,14 +116,46 @@ const decompressNum = (str: string) => {
     return num;
 };
 
+/**
+ * Description placeholder
+ * @date 1/25/2024 - 4:58:48 PM
+ *
+ * @export
+ * @class Trace
+ * @typedef {Trace}
+ */
 export class Trace {
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @param {TraceArray} trace
+     * @returns {*}
+     */
     static encode(trace: TraceArray) {
         return trace.map(Trace.compress);
     }
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @param {string[]} trace
+     * @returns {*}
+     */
     static decode(trace: string[]) {
         return trace.map(Trace.decompress);
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @param {P} point
+     * @returns {string}
+     */
     static compress(point: P): string {
         const [i, x, y, a] = point;
         // i is a whole number between 0 and 600
@@ -68,6 +172,14 @@ export class Trace {
         return [iStr, xStr, yStr, aStr].join(' ');
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @param {string} str
+     * @returns {P}
+     */
     static decompress(str: string): P {
         const [iStr, xStr, yStr, aStr] = str.split(' ');
         const i = decompressI(iStr);
@@ -78,35 +190,63 @@ export class Trace {
         return [i, x, y, a];
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @param {TraceArray} trace1
+     * @param {TraceArray} trace2
+     * @returns {| {
+     *             status: 'incorrect-length';
+     *             l1: number;
+     *             l2: number;
+     *         }
+     *         | {
+     *             status: 'incorrect-point';
+     *             i: number;
+     *             p1: P;
+     *             p2: P;
+     *         }
+     *         | {
+     *             status: 'incorrect-action';
+     *             i: number;
+     *             a1: Action | 0;
+     *             a2: Action | 0;
+     *         }
+     *         | {
+     *             status: 'identical';
+     *         }}
+     */
     static compare(
         trace1: TraceArray,
-        trace2: TraceArray,
+        trace2: TraceArray
     ):
         | {
-            status: 'incorrect-length';
-            l1: number;
-            l2: number;
-        }
+              status: 'incorrect-length';
+              l1: number;
+              l2: number;
+          }
         | {
-            status: 'incorrect-point';
-            i: number;
-            p1: P;
-            p2: P;
-        }
+              status: 'incorrect-point';
+              i: number;
+              p1: P;
+              p2: P;
+          }
         | {
-            status: 'incorrect-action';
-            i: number;
-            a1: Action | 0;
-            a2: Action | 0;
-        }
+              status: 'incorrect-action';
+              i: number;
+              a1: Action | 0;
+              a2: Action | 0;
+          }
         | {
-            status: 'identical';
-        } {
+              status: 'identical';
+          } {
         if (trace1.length !== trace2.length) {
             return {
                 status: 'incorrect-length',
                 l1: trace1.length,
-                l2: trace2.length,
+                l2: trace2.length
             };
         }
 
@@ -119,7 +259,7 @@ export class Trace {
                     status: 'incorrect-point',
                     i,
                     p1,
-                    p2,
+                    p2
                 };
             }
 
@@ -128,16 +268,26 @@ export class Trace {
                     status: 'incorrect-action',
                     i,
                     a1: p1[3],
-                    a2: p2[3],
+                    a2: p2[3]
                 };
             }
         }
 
         return {
-            status: 'identical',
+            status: 'identical'
         };
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @param {P} p
+     * @param {number} i
+     * @param {P[]} a
+     * @returns {boolean}
+     */
     static filterPipe(p: P, i: number, a: P[]) {
         if (p[3] !== 0 && a[i - 1]) {
             const x1 = a[i - 1][1];
@@ -152,10 +302,27 @@ export class Trace {
         return p[1] !== -1 && p[2] !== -1;
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @template [T=Action]
+     * @param {T} action
+     * @returns {(p: P) => boolean}
+     */
     static filterAction<T = Action>(action: T) {
         return (p: P) => p[3] === action;
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @readonly
+     * @type {{ map: (trace: {}) => {}; total: (trace: {}) => any; }}
+     */
     static get distance() {
         return {
             map: (trace: TraceArray) => {
@@ -173,16 +340,24 @@ export class Trace {
 
                         return distance;
                     })
-                    .filter((p) => p !== null) as number[];
+                    .filter(p => p !== null) as number[];
             },
             total: (trace: TraceArray) => {
                 const map = Trace.distance.map(trace);
 
                 return map.reduce((a, b) => a + b, 0);
-            },
+            }
         };
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @readonly
+     * @type {{ map: (trace: {}) => {}; histogram: (trace: {}) => {}; average: (trace: {}) => number; }}
+     */
     static get velocity() {
         return {
             map: (trace: TraceArray) => {
@@ -200,7 +375,7 @@ export class Trace {
 
                         return distance / 0.25;
                     })
-                    .filter((p) => p !== null) as number[];
+                    .filter(p => p !== null) as number[];
             },
             histogram: (trace: TraceArray) => {
                 const map = Trace.velocity.map(trace);
@@ -212,7 +387,7 @@ export class Trace {
 
                 const buckets = new Array(10).fill(0) as number[];
 
-                map.forEach((v) => {
+                map.forEach(v => {
                     const bucket = Math.floor(((v - min) / range) * 10);
                     buckets[bucket]++;
                 });
@@ -223,10 +398,18 @@ export class Trace {
                 const map = Trace.velocity.map(trace);
 
                 return map.reduce((a, b) => a + b, 0) / map.length;
-            },
+            }
         };
     }
 
+    /**
+     * Description placeholder
+     * @date 1/25/2024 - 4:58:48 PM
+     *
+     * @static
+     * @readonly
+     * @type {{ compress: (str: string | number) => string; decompress: (str: string) => string; encode: (trace: {}) => {}; decode: (trace: {}) => any; }}
+     */
     static get old() {
         const chars =
             'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|:"<>?`~[]\';./=\\,';
@@ -235,7 +418,7 @@ export class Trace {
             return [
                 +str.slice(0, 2) / 100,
                 +str.slice(2, 4) / 100,
-                +str.slice(4, 10) / 1000,
+                +str.slice(4, 10) / 1000
             ];
         };
 
@@ -254,7 +437,8 @@ export class Trace {
                 const base = chars.length;
                 let num = 0;
                 for (let i = 0; i < str.length; i++) {
-                    num += chars.indexOf(str[i]) *
+                    num +=
+                        chars.indexOf(str[i]) *
                         Math.pow(base, str.length - i - 1);
                 }
                 str = num.toString();
@@ -262,13 +446,13 @@ export class Trace {
                 return new Array(10 - str.length).fill('0').join('') + str;
             },
             encode: (
-                trace: [string | number, string | number, string | number][],
+                trace: [string | number, string | number, string | number][]
             ): string[] => {
-                return trace.map((p) => p.map(Trace.old.compress).join(' '));
+                return trace.map(p => p.map(Trace.old.compress).join(' '));
             },
             decode: (trace: string[]) => {
                 return trace.map(Trace.old.decompress);
-            },
+            }
         };
     }
 }
