067993f00abd001cc179c293e067dbf6be95be0a
Sambews <ton.of.keys@gmail.com>
2024-01-29 21:08:57 -0700

    Made match selector component with buttons to select match

---
+++ client/views/components/MatchSelector.svelte
@@ -0,0 +1,14 @@
+<div class="btn-group" role="group" aria-label="Basic mixed styles example">
+    <button type="button" class="btn btn-danger">
+        <i class="material-icons">chevron_left</i>
+        Previous Match
+    </button>
+    <button type="button" class="btn btn-warning">
+        Play Match
+        <i class="material-icons">play_arrow</i>
+    </button>
+    <button type="button" class="btn btn-success">
+        Next Match
+        <i class="material-icons">chevron_right</i>
+    </button>
+</div>
