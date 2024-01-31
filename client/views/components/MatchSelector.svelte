4244fd5d4ea7ebcddafd4159479a4a1df91c4750
Sambews <ton.of.keys@gmail.com>
2024-01-31 12:12:06 -0700

    Added buttons for custom info, tutorial, assigned teams, and field orientation, and added text input for the scout's name. Buttons only, no functionality

--- client/views/components/MatchSelector.svelte
+++ client/views/components/MatchSelector.svelte
@@ -1,13 +1,13 @@
-<div class="btn-group" role="group" aria-label="Basic mixed styles example">
+<div class="btn-group w-100" role="group">
     <button type="button" class="btn btn-danger">
         <i class="material-icons">chevron_left</i>
         Previous Match
     </button>
-    <button type="button" class="btn btn-warning">
+    <button type="button" class="btn btn-success">
         Play Match
         <i class="material-icons">play_arrow</i>
     </button>
-    <button type="button" class="btn btn-success">
+    <button type="button" class="btn btn-primary">
         Next Match
         <i class="material-icons">chevron_right</i>
     </button>
---
+++ client/views/components/ScoutSelect.svelte
@@ -0,0 +1,1 @@
+<input class='w-100' placeholder="Scout Name">
