782a4bdc3cb501d33175a8eeb6da1420a550386e
Sambews <ton.of.keys@gmail.com>
2024-02-05 19:18:39 -0700

    Added a insert custom info button

--- client/views/components/MatchSelector.svelte
+++ client/views/components/MatchSelector.svelte
@@ -1,14 +1,21 @@
 <div class="btn-group w-100" role="group">
-    <button type="button" class="btn btn-danger">
+    <button type="button" class="btn btn-danger" on:click = {() => sayAction('previous match')}>
         <i class="material-icons">chevron_left</i>
         Previous Match
     </button>
-    <button type="button" class="btn btn-success">
+    <button type="button" class="btn btn-success" on:click = {() => sayAction('playing the match')}>
         Play Match
         <i class="material-icons">play_arrow</i>
     </button>
-    <button type="button" class="btn btn-primary">
+    <button type="button" class="btn btn-primary" on:click = {() => sayAction('nexting the match')}>
         Next Match
         <i class="material-icons">chevron_right</i>
     </button>
 </div>
+
+<script lang='ts'>
+    const sayAction = (action: string) => {
+        console.log('You did ' + action)
+    }
+
+</script>
