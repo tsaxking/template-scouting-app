99f5f3fe8383331660783a6f28cec197733eab09
Taylor King <taylorking206@u.boisestate.edu>
2024-02-07 03:54:27 -0700

    fix stdin

--- server/utilities/utilties.ts
+++ server/utilities/utilties.ts
@@ -1,19 +1,19 @@
 import { EventEmitter } from '../../shared/event-emitter.ts';
+import { readLines } from 'https://deno.land/std@0.100.0/io/mod.ts';
 
 export const stdin = new EventEmitter();
 (async () => {
     if (!Deno.args.includes('--allow-stdin')) return;
-    const decoder = new TextDecoder();
-
+    const lines = readLines(Deno.stdin);
     const close = () => Deno.stdin.close();
 
-    stdin.on('close', close);
-
     Deno.addSignalListener('SIGINT', close);
+    Deno.addSignalListener('SIGTERM', close);
+    Deno.addSignalListener('SIGHUP', close);
+    Deno.addSignalListener('SIGQUIT', close);
 
-    for await (const chunk of Deno.stdin.readable) {
-        const text = decoder.decode(chunk);
-        stdin.emit('data', text);
-        stdin.emit(text);
+    for await (const line of lines) {
+        stdin.emit(line);
+        stdin.emit('data', line);
     }
 })();
