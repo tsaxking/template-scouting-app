13905fe87a049af562cc7e62cd652e08ad946754
tsaxking <taylorking206@u.boisestate.edu>
2024-02-11 04:20:58 -0700

    dispatch event

--- client/views/components/main/UploadPicture.svelte
+++ client/views/components/main/UploadPicture.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-    import { Modal } from '../../../utilities/modals';
     import { Random } from '../../../../shared/math';
+    import { abbreviate } from '../../../../shared/text';
+    import { createEventDispatcher } from 'svelte';
     const id = 'input-' + Random.uuid();
     let input: HTMLInputElement;
     export let multiple: boolean = true;
 
-    let pictures: string[] = [];
+    type P = {
+        name: string;
+        url: string;
+    }
+
+    let pictures: P[] = [];
+
+    const dispatch = createEventDispatcher();
+
+    $: dispatch('change', pictures);
 
     const fns = {
         onInput: async (e: Event) => {
@@ -13,13 +23,19 @@
             if (files) {
                 pictures = [
                 ...(multiple ? pictures : []),
-                ...await Promise.all(Array.from(files).map(f => new Promise<string>((res, rej) => {
+                ...await Promise.all(Array.from(files).map(f => new Promise<P>((res, rej) => {
                     const reader = new FileReader();
-                    reader.onload = () => res(reader.result as string);
+                    reader.onload = async() => res({
+                        name: f.name,
+                        url: reader.result as string
+                    });
                     reader.onerror = rej;
                     reader.readAsDataURL(f);
                 })))];
             }
+        },
+        remove: (p: P) => {
+            pictures = pictures.filter(p2 => p2 !== p);
         }
     };
 </script>
@@ -29,14 +45,19 @@
         <div class="col-md-6">
             <input type="file" name="" id={id} class="form-control" {multiple} bind:this={input} on:change={fns.onInput} accept=".png,.PNG,.jpg,.JPG,.jpeg,.JPEG">
         </div>
-        <div class="col-md-6">
-            <button type="button" class="btn btn-primary">Preview</button>
-        </div>
     </div>
     <div class="row mb-3">
         {#each pictures as picture, i}
-            <div class="col">
-                <img src={picture} alt="picture {i}" class="img-thumbnail">
+            <div class="col-sm-4 col-md-3 col-lg-2 m-1 no-select">
+                <div class="d-flex justify-content-between align-items-center py-1">
+                    <p class="p-0 m-0">
+                        {abbreviate(picture.name, 20)}
+                    </p>
+                    <i class="material-icons text-danger cursor-pointer p-0 m-0" on:click={() => fns.remove(picture)}>
+                        close
+                    </i>
+                </div>
+                <img src={picture.url} alt="picture {i}" class="img-thumbnail p-1">
             </div>
         {/each}
     </div>
