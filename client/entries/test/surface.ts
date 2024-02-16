fa6fba63db51aeb24a103e97bd03c0425206d977
Taylor King <taylorking206@u.boisestate.edu>
2024-02-16 12:28:08 -0700

    spline sim

---
+++ client/entries/test/surface.ts
@@ -0,0 +1,34 @@
+import '../../utilities/imports';
+
+import { Canvas } from '../../models/canvas/canvas';
+import { Surface } from '../../models/canvas/surface';
+
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+document.body.classList.add('no-scroll');
+
+const c = new Canvas(canvas.getContext('2d')!);
+
+const {
+    sin,
+    PI,
+    cos
+} = Math;
+
+const s = new Surface(
+    (x, y) => cos(PI * x) * sin(PI * y),
+    100,
+    [[.5, .4, .5], [0.7, 1, .9], [.6, 0.2, .1]]
+);
+
+console.log(s.triangles);
+
+c.add(s);
+
+
+c.draw();
