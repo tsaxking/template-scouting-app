3071c3b3ab9fccefc67bbf50673d1025b1e81523
tsaxking <taylorking206@u.boisestate.edu>
2024-02-28 15:22:36 -0700

    deps

--- client/utilities/imports.ts
+++ client/utilities/imports.ts
@@ -1,21 +1,7 @@
-// These imports are for all client side views
-// import "https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css";
-// import "https://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css";
-// import "https://fonts.googleapis.com/icon?family=Material+Icons";
-// import "https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css";
-// import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";
-// import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css";
-// import "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js";
-// import "https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js";
-// import "https://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js";
-// import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js";
-// import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js";
-
 import '../styles/animate.css';
 import '../styles/bootstrap-colors-extended.css';
 import '../styles/global.css';
 import '../styles/style.css';
-import './deps';
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
