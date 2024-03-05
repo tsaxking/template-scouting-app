36c3b50c6f4958dd4891a30335a69b32bec981a6
tsaxking <taylorking206@u.boisestate.edu>
2024-03-04 18:40:16 -0700

    start app

--- new-server/structure/app/app.ts
+++ new-server/structure/app/app.ts
@@ -1,3 +1,165 @@
 import express from 'express';
 // import { __root } from '../../utilities/env.ts';
 import path from 'path';
+import { log } from '../../../server/utilities/terminal-logging';
+
+/**
+ * All file types that can be sent (can be expanded)
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @export
+ * @typedef {FileType}
+ */
+export type FileType =
+    | 'js'
+    | 'css'
+    | 'html'
+    | 'json'
+    | 'png'
+    | 'jpg'
+    | 'jpeg'
+    | 'gif'
+    | 'svg'
+    | 'ico'
+    | 'ttf'
+    | 'woff'
+    | 'woff2'
+    | 'otf'
+    | 'eot'
+    | 'mp4'
+    | 'webm'
+    | 'mp3'
+    | 'wav'
+    | 'ogg'
+    | 'txt'
+    | 'pdf'
+    | 'zip'
+    | 'rar'
+    | 'tar'
+    | '7z'
+    | 'xml'
+    | 'doc'
+    | 'docx'
+    | 'xls'
+    | 'xlsx'
+    | 'ppt'
+    | 'pptx'
+    | 'avi'
+    | 'wmv'
+    | 'mov'
+    | 'mpeg'
+    | 'flv';
+
+/**
+ * Enum for response status
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @export
+ * @enum {number}
+ */
+export enum ResponseStatus {
+    fileNotFound,
+    success,
+    error,
+}
+
+/**
+ * Options to apply to cookies
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @export
+ * @typedef {CookieOptions}
+ */
+export type CookieOptions = {
+    maxAge?: number;
+    httpOnly?: boolean;
+    secure?: boolean;
+    domain?: string;
+    path?: string;
+    sameSite?: 'Strict' | 'Lax' | 'None';
+};
+
+/**
+ * All of the request methods that are supported
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @enum {number}
+ */
+enum RequestMethod {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    USE = 'USE',
+}
+
+/**
+ * "Next" function that is called to move to the next middleware function
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @export
+ * @typedef {Next}
+ */
+export type Next = () => void;
+
+/**
+ * Server function that is called when a request is made
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @export
+ * @typedef {ServerFunction}
+ */
+export type ServerFunction<T = unknown> = (
+    req: Req<T>,
+    res: Res,
+    next: Next,
+) => any | Promise<any>;
+/**
+ * Final function that is called at the end of a request
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @export
+ * @typedef {FinalFunction}
+ */
+export type FinalFunction<T> = (req: Req<T>, res: Res) => any;
+
+/**
+ * Options for starting the application
+ * @date 10/12/2023 - 2:49:37 PM
+ *
+ * @typedef {AppOptions}
+ */
+type AppOptions = {
+    onListen?: (server: Deno.Server) => void;
+    onConnection?: (socket: any) => void;
+    onDisconnect?: () => void;
+    ioPort?: number;
+    blockedIps?: string[];
+};
+
+export class App {
+    public static headerAuth(key: string, value: string): ServerFunction {
+        return (req, res, next) => {
+            if (req.headers.get(name) === value){
+                next();
+            } else {
+                res.sendStatus('permissions:unauthorized');
+            }
+        }
+    }
+
+    public readonly io: SocketWrapper;
+    public readonly server: express.Application;
+
+    constructor(
+        public readonly port: number,
+        public readonly domain: string,
+        options?: Partial<AppOptions> 
+    ) {
+        this.server = express();
+
+        this.server.listen(port, () => {
+            log(`Server is listening on port ${port}`);
+        });
+    }
+}
