c144816f42b369c35fdbad97bc6767ee5e1be514
tsaxking <taylorking206@u.boisestate.edu>
2024-03-07 01:46:26 -0700

    use full screen

--- client/views/components/main/Navbar.svelte
+++ client/views/components/main/Navbar.svelte
@@ -42,13 +42,11 @@ const fns = {
 >
     <div class="d-inline-flex p-0" style="height: 42px;">
         <button
-            class="btn btn-dark navbar-toggler border-0"
+            class="btn btn-dark border-0"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#sidebar-nav"
             aria-controls="sidebar-nav"
-            aria-expanded="false"
-            aria-label="Toggle Side Bar"
         >
             <i class="bi bi-layout-sidebar"></i>
         </button>
