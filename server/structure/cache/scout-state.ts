cbec48b2d2f00ab1d12bc840365930fc736f2519
tsaxking <taylorking206@u.boisestate.edu>
2024-06-27 23:17:22 -0600

    illegal constructor

--- server/structure/cache/scout-state.ts
+++ server/structure/cache/scout-state.ts
@@ -2,4 +2,12 @@ import { Cache } from "./cache";
 import { Tablet } from "./tablet";
 
 
-export class AppState extends Cache {}
+export class AppState extends Cache {
+    static tablets: Tablet[] = [];
+
+
+    constructor() {
+        super();
+        throw new Error('Illegal constructor call');
+    }
+}
