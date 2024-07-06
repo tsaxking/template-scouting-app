8a2b39ce55a3160bcba7172a37795635679a0c59
Taylor King <taylorking206@u.boisestate.edu>
2024-07-06 05:41:25 -0600

    use loop() instead of requestAnimationFrame since node.js does not have that

--- shared/loop.ts
+++ shared/loop.ts
@@ -50,10 +50,11 @@ export class Loop<
             const end = Date.now();
             const diff = end - start;
             const wait = Math.max(0, this.interval - diff);
+            console.log(wait);
             await sleep(wait);
-            requestAnimationFrame(loop);
+            loop();
         };
 
-        requestAnimationFrame(loop);
+        loop();
     }
 }
