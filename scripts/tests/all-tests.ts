286951b06b55d9e70209d15cca992db481258ef5
Taylor King <taylorking206@u.boisestate.edu>
2024-08-19 01:30:10 -0600

    change how backend tests work

--- scripts/tests/all-tests.ts
+++ scripts/tests/all-tests.ts
@@ -6,14 +6,33 @@ import { log } from '../../server/utilities/terminal-logging';
 import { validate } from '../../server/middleware/data-type';
 import { Req } from '../../server/structure/app/req';
 import { Res } from '../../server/structure/app/res';
-import test from 'test';
-import assert from 'assert';
+import { deepEqual } from 'assert';
 
 const assertEquals = (a: unknown, b: unknown) => {
-    assert.deepEqual(a, b);
-};
+    try {
+        deepEqual(a, b);
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
 
-export const runTests = async () => {
+
+const test = async (name: string, fn: () => void | Promise<void>) => {
+    const ok = '✅';
+    const fail = '❌';
+    try {
+        await fn();
+        console.log(ok, name);
+        return 0;
+    } catch (e) {
+        console.log(fail, name);
+        console.error(e);
+        return 1;
+    }
+}
+
+export const runTests = async () => Promise.all([
     test('Run async task functionality', async () => {
         const asyncTest = await runFile<string[]>(
             './scripts/tests/run-task-test.ts',
@@ -25,7 +44,7 @@ export const runTests = async () => {
         log('Async test result:', asyncTest);
         if (asyncTest.isErr()) throw asyncTest.error;
         else assertEquals(asyncTest.value, ['a', 'b', 'c']);
-    });
+    }),
 
     test('Run sync task functionality', async () => {
         const syncTest = await runFile<string[]>(
@@ -38,14 +57,14 @@ export const runTests = async () => {
         log('Sync test result:', syncTest);
         if (syncTest.isErr()) throw syncTest.error;
         else assertEquals(syncTest.value, ['a', 'b', 'c']);
-    });
+    }),
 
     test('Run command', async () => {
         const result = await runTask('echo', ['"test"']);
         log('Command result:', result);
         if (result.isOk()) return assertEquals(true, true);
         throw result.error;
-    });
+    }),
 
     test('Data validation', async () => {
         const fail = () => {
@@ -138,13 +157,13 @@ export const runTests = async () => {
             } as unknown as Res,
             fail
         );
-    });
+    }),
 
     // if (!process.argv.includes('lite')) {
     //     test('Database tests', async () => {
     //         const { DB } = await import('../../server/utilities/databases');
     //     });
     // }
-};
+]);
 
-if (require.main) runTests();
+if (require.main === module) runTests().then((val) => process.exit(val.some(v => v === 1) ? 1 : 0));
