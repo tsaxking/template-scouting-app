1047ca2b822a9658e05deed8dd339f688301067c
taylor <taylorking206@u.boisestate.edu>
2024-09-23 19:12:19 -0600

    fix build system

--- client/entries/test/3d-graph.ts
+++ client/entries/test/3d-graph.ts
@@ -11,7 +11,7 @@ import {
     translate
 } from '../../../shared/submodules/calculations/src/linear-algebra/matrix-calculations';
 import { Color } from '../../submodules/colors/color';
-import { Spline } from '../../../shared/submodules/calculations/src/linear-algebra/spline';
+import { BezzierSpline as Spline } from '../../../shared/submodules/calculations/src/linear-algebra/spline';
 
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
