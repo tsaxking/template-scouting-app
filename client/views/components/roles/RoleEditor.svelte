55fb4b1b0bd1a9477956aff284136a2408f5b31d
taylor <taylorking206@u.boisestate.edu>
2024-12-02 01:38:41 -0700

    start role table

---
+++ client/views/components/roles/RoleEditor.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { Permissions } from "../../../models/permissions";
+
+    export let role: Permissions.RoleData;
+</script>
---
+++ client/views/components/roles/RoleTable.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { Permissions } from "../../../models/permissions";
+import RoleEditor from "./RoleEditor.svelte";
+import Modal from "../bootstrap/Modal.svelte";
+
+    const rolesStore = Permissions.Role.all(true);
+
+    const roles = $rolesStore;
+
+    let selected: Permissions.RoleData | null = null;
+    let showEditor = false;
+    $: showEditor = !!selected;
+</script>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>Role</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each roles as role (role.id)}
+            <tr on:click={() => {
+                selected = role;
+            }}>
+                <td>{role.data.name}</td>
+                <td>{role.data.description}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+{#if selected} 
+    <Modal 
+        bind:show={showEditor}
+        title={'Role: ' + selected.data.name} 
+    >
+        <RoleEditor role={selected} />
+    </Modal>
+{/if}
